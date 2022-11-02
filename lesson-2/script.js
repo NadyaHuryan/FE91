97e6990cc04a435d566d5c6ae8da146edf7b18e8
Roman Glinnik <roma.glinnik@gmail.com>
2022-11-03 00:44:57 +0300

    hw_2: fix some remarks

--- lesson-2/script.js
+++ lesson-2/script.js
@@ -1,50 +1,24 @@
 //TASK 1
 
-let height = 15
-let width = 20
+let height = 15;
+let width = 15;
 
 if (height > width) {
     console.log("наибольшая сторона высота и ее размер " + height);
-} else {
+} else if (height < width) {
 console.log("наибольшая сторона ширина и ее размер " + width);
+} else {
+    console.log("они равны " + width);
 }
 
 //TASK 2
 
 let sum = 0;
 
-for (i = 0; i < 6; i++) {
+for (i = 1; i < 6; i++) {
    sum = sum + i;
+   console.log(sum)
 }
-console.log(sum)
-
-sum = 0;
-
-for (i = 0; i < 5; i++) {
-   sum = sum + i;
-}
-console.log(sum)
-
-sum = 0;
-
-for (i = 0; i < 4; i++) {
-   sum = sum + i;
-}
-console.log(sum)
-
-sum = 0;
-
-for (i = 0; i < 3; i++) {
-    sum = sum + i;
- }
- console.log(sum)
-
-sum = 0;
-
- for (i = 0; i < 2; i++) {
-    sum = sum + i;
- }
- console.log(sum)
 
 //TASK 3
 
@@ -61,13 +35,13 @@ for (i = 1; i < 21; i++) {
  let pen = true
  let apple = false
  let orange = true
- let shouldGoToWork;
+ let shouldGoToWork = key && documents && pen && (apple || orange);
 
-if (key && documents && pen && (apple || orange)) {
-    shouldGoToWork = "Можно идти";
+if (shouldGoToWork) {
+    console.log("Можно идти");
 } else {
-    shouldGoToWork = "Что-то забыл";
-} console.log(shouldGoToWork);
+    console.log("Что-то забыл");
+} 
 
 //TASK 5
 
@@ -89,9 +63,10 @@ let age = prompt("Введите ваш возраст");
 
 if (age > 17) {
     console.log("Доступ разрешен");
-} else if ((age == 16) || (age == 17)) {
-    console.log("Уже почти");
-} else {
+}  else {
+    if ((age == 16) || (age == 17)) {
+        console.log("Уже почти");
+    }
     console.log("Доступ запрещен");
 }
 
@@ -117,6 +92,7 @@ switch (arg) {
 
   default:
     console.log( "Неизвестное значение, попробуйте ещё раз" );
+    break;
 }
 
 //ADVANCED TASK 1
@@ -125,11 +101,11 @@ let summ = 0;
 let count = 0;
 let num;
 
-while (( num = prompt("Введите число"), 0), num !== "0" && num !== "") {
+while (( num = prompt("Введите число"), 0), num !== "0" && num !== "" && !isNaN(num)) {
     count++;
     summ = summ + +num;
-    console.log("Сумма чисел: " + summ + " Среднее арифметическое: " + (summ / count));
 }
+console.log("Сумма чисел: " + summ + " Среднее арифметическое: " + (summ / count));
 
 //ADVANCED TASK 2
 
@@ -139,13 +115,16 @@ let num3 = prompt("Сколько прибавить?");
 let num4 = prompt("Сколько умножить?");
 let num5 = prompt("Сколько разделить?");
 
-console.log("((((" + num1 + " - " + num2 + ")" + " + " + num3 + ")" + " * " + num4 + ")" + " / " + num5 + " = " + (((((num1 - num2) + +num3) * num4) / num5)));
+let total = (((((num1 - num2) + +num3) * num4) / num5));
+
+console.log("((((" + num1 + " - " + num2 + ")" + " + " + num3 + ")" + " * " + num4 + ")" + " / " + num5 + " = " + total);
 
 //ADVANCED TASK 3
 
 let str = "#";
+let len = prompt("Введите длину");
 
-while (str.length < 7) {
+while (str.length <= len) {
     console.log(str);
     str = str + "#";
 }
