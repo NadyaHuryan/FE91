b125d1a5ba7a3d584ed216fb26073bac9f5a17a6
andreykalenik <kalenik.andrey@inbox.ru>
2022-11-08 14:15:51 +0300

    NORMAL level || task 1 and task 2 done

---
+++ lesson-4 array/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const colors = ['red', 'green', 'blue'];
+console.log(colors.length);
+//task 1  NORMAL level 
+
+
+const animals = ['monkey', 'dog', 'cat'];
+console.log(colors[colors.length-1]);   //old version
+console.log(colors.at(-1));             // new version
+//task 2  NORMAL level 
