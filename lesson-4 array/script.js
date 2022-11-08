c519579626e3f3dca707b0468322d73d397f0757
andreykalenik <kalenik.andrey@inbox.ru>
2022-11-08 17:53:01 +0300

    NORMAL level || task 3 done

--- lesson-4 array/script.js
+++ lesson-4 array/script.js
@@ -9,3 +9,16 @@ const animals = ['monkey', 'dog', 'cat'];
 console.log(colors[colors.length-1]);   //old version
 console.log(colors.at(-1));             // new version
 //task 2  NORMAL level 
+
+const numb = [5, 43, 63, 23, 90];
+
+//version 1
+for (let i = 0; i < numb.length; i++) {
+    console.log(numb[i]);
+  }
+
+//version 2
+for (let j of numb) {
+    console.log( j );
+  }  
+  //task 3  NORMAL level 
