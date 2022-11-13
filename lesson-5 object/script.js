fba2b8de79e9279065abab8a539128829a15845d
Andrey Kalenik <kalenik.andrey@inbox.ru>
2022-11-13 20:48:13 +0300

    NORMAL level || task 3 done

--- lesson-5 object/script.js
+++ lesson-5 object/script.js
@@ -7,10 +7,29 @@ delete something.name;
 delete something.status;
 // task 1 NORMAL level
 
-const student = {
+
+
+const men = {
     name : "Valera",
     age: 19,
 }
 
-console.log ("name" in student);
+console.log ("name" in men);
 // task 2 NORMAL level
+
+
+
+const student = {
+    name: 'John',
+    age: 19,
+    isHappy: true
+}
+
+for (key in student){
+    console.log(key);
+}
+
+for (key in student){
+    console.log(student[key]);
+}
+// task 3 NORMAL level
