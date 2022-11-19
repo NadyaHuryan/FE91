93d2cb67ea7ec7bb06cc437e26fb6556d74102b8
Andrey Kalenik <kalenik.andrey@inbox.ru>
2022-11-19 21:31:23 +0300

    NORMAL level || task 2 done

--- lesson-6 array + func/script.js
+++ lesson-6 array + func/script.js
@@ -1,20 +1,28 @@
 const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
 
+fibonacci.forEach(function(item) {
+    console.log(`${item}`);
+    });
 
-function showNumber1 (arr){
-    arr.forEach(function(item) {
-        console.log(`${item}`);
-        });
-};
-function showNumber2 (arr){
-    arr.forEach(item  => console.log(`${item}`));
-};
-// полустрелочная функция, не понимаю как сделать стрелочную облочку для forEach
-
-showNumber1(fibonacci);
 console.log('---------')
-showNumber2(fibonacci);
 
+fibonacci.forEach(item  => console.log(item));
+console.log('##########')
 //task 1 NORMAL level
 
 
+
+
+const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton'];
+
+let remapUsers1 = users.map(function(item, index) {
+    return "member " + (index + 1) + ": "+ item;
+  })
+
+let remapUsers2 = users.map((item, index) => "member " + (index + 1) + ": "+ item);
+
+console.log(remapUsers1);
+console.log('---------')
+console.log(remapUsers2);
+console.log('##########')
+//task 2 NORMAL level
