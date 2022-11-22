44121f2448b6a3d115991c37e4f16c1c09dd56fd
xxaosens <vlad.obito@mail.ru>
2022-11-22 12:22:02 +0300

    hw-2

--- lesson-2/script.js
+++ lesson-2/script.js
@@ -1,27 +1,27 @@
 //////////////////////Задание 1///////////////////////
 let height = 35
 let width = 30
-if (height > width){
-alert(height);
+if (height > width) {
+    console.log(height);
 }
 else {
-alert(width);
+    console.log(width);
 };
 //Работает//
 //////////////////////Задание 2///////////////////////
 let x = prompt('Введите число'),
-sum = 0;
+    sum = 0;
 
 for (let i = 0; i <= x; i++) {
-sum += i;
+    sum += i;
+    console.log(sum);
 }
-console.log(sum);
 //Работает//
 //////////////////////Задание 3///////////////////////
 for (i = 1; i <= 20; i++) {
-if (i % 3 == 0) {
-console.log(i)
-}
+    if (i % 3 == 0) {
+        console.log(i)
+    }
 }
 //Работает//
 // //////////////////////Задание 4///////////////////////
@@ -44,50 +44,51 @@ let apple = false
 let orange = true;
 
 if (key && documents && pen && (pen || orange)) {
-console.log("Готов");
+    console.log("Готов");
 }
-else{console.log("Не Готов");}
+else { console.log("Не Готов"); }
 //Работает//
 //////////////////////Задание 5///////////////////////
 let namber = prompt('Введите число');
-if (namber % 3 == 0) {
-alert("Fiz");
+if (namber % 5 == 0 && namber % 3 == 0) {
+    alert("FizBuz");
 } else if (namber % 5 == 0) {
-alert("Buz");
-} else if (namber % 5 == 0 && namber % 3 == 0){
-alert("FizBuz");
+    alert("Buz");
+} else if (namber % 3 == 0) {
+    alert("Fiz");
 } else {
-alert("Введите другое число");
+    alert("Введите другое число");
 }
-//Не показывает FizBuz//
 //////////////////////Задание 6///////////////////////
 let age = prompt("Ваш возраст");
 if (age >= 18) {
-alert("Доступ разрешен");
-} else if (age > 15 && age < 18){
-alert("Уже почти");
+    alert("Доступ разрешен");
+} else if (age > 15 && age < 18) {
+    alert("Уже почти");
 } else {
-alert("Доступ запрещен")
+    alert("Доступ запрещен")
 }
 //Работает//
 // //////////////////////Задание 7///////////////////////
 let side = prompt("В какую сторону света хотите отправиться?");
 switch (side) {
-case "юг":
-alert("на юг пойдешь счастье найдешь");
-break;
+    case "юг":
+        alert("на юг пойдешь счастье найдешь");
+        break;
 
-case "север":
-alert("на север пойдешь много денег найдешь");
-break;
+    case "север":
+        alert("на север пойдешь много денег найдешь");
+        break;
 
-case "запад":
-alert("на запад пойдешь верного друга найдешь");
-break;
+    case "запад":
+        alert("на запад пойдешь верного друга найдешь");
+        break;
 
-case "восток":
-alert("на восток пойдешь разработчиком станешь");
-break;
+    case "восток":
+        alert("на восток пойдешь разработчиком станешь");
+        break;
+    default:
+        alert("Нет таких значений");
 }
 //Работает//
 //////////////////////Задание 01///////////////////////
@@ -111,9 +112,10 @@ console.log("((((" + namber1 + " - " + namber2 + ")" + " + " + namber3 + ")" + "
 
 //Работает//
 //////////////////////Задание 03///////////////////////
-let grid = "#"
-sum = "";
-for (i = 0; i < 10; i++) {
-    console.log(sum += grid)
+let grid = "#";
+let sum1 = "";
+let n = 10;
+for (i = 0; i < n; i++) {
+    console.log(sum1 += grid)
 }
 //Работает//
