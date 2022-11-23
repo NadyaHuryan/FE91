18cca72f60f3055c0828cecdd0844b834d3f3289
Vorobyshek228 <ivanobych@gmail.com>
2022-11-24 00:00:11 +0300

    init hw_6

---
+++ lesson-6 array + func/script.js
@@ -0,0 +1,12 @@
+//1 Task
+//function declaretion
+const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+fibonacci.forEach(function(elem){
+    console.log(elem);
+    return elem;
+});
+//Arrow
+fibonacci.forEach(elem => console.log(elem));
+
+//2 Task
+
