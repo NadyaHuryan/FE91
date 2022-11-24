71d6212d088cfc4ef323d2fe033dc7bae1508ab6
Roman Glinnik <roma.glinnik@gmail.com>
2022-11-24 14:16:27 +0300

    Done some advanced tasks

--- lesson-6 array + func/script.js
+++ lesson-6 array + func/script.js
@@ -67,3 +67,98 @@ let even1 = numbers1.find(function(item) {
 console.log(even1);
 
 //ADVANCED TASK 1
+
+function Student(name, salary, rate) {
+    this.name = name;
+    this.salary = salary;
+    this.rate = rate;
+    this.getCredit = function(){
+        let credit = 0;
+        if (this.rate === "A") {
+            credit = this.salary * 12;
+            return credit;
+        } else if (this.rate === "B") {
+            credit = this.salary * 9;
+            return credit;
+        } else if (this.rate === "C") {
+            credit = this.salary * 6;
+            return credit;
+        } else {
+            return credit = 0;
+        }
+    }
+  }
+
+  let student1 = new Student("Roma", 500, "C");
+  let student2 = new Student("Dima", 600, "A");
+  let student3 = new Student("Ivan", 700, "B");
+  let student4 = new Student("Aliona", 800, "D");
+  let student5 = new Student("Sergei", 900, "A");
+
+  let students = [student1, student2, student3, student4, student5];
+  
+  function getCreditSum(students) {
+    let sum = 0;
+    for (let item in students) {
+        sum += students[item].getCredit();
+    }
+    return sum;
+  }
+
+  console.log(getCreditSum(students));
+
+  //ADVANCED TASK 2
+
+  function getStrWithoutVowels (str) {
+    let vowels = ["a", "A", "e", "E", "u", "U", "i", "I", "o", "O", ];
+    let newArr = str.split("").filter(function(item) {
+        return !vowels.includes(item);
+    });
+    return newArr.join("");
+  }
+
+  console.log(getStrWithoutVowels("This website is for losers LOL!"));
+
+  //ADVANCED TASK 3
+
+
+
+  //ADVANCED TASK 4
+
+  function highAndLow(str) {
+    let strArray = str.split(" ");
+    strArray.sort(function(a, b) {
+        return a - b;
+    });
+    for (let item in strArray) {
+        max = strArray[strArray.length - 1];
+        min = strArray[0];
+    } 
+    console.log(`${max} ${min}`);
+  }
+
+  highAndLow("1 2 3 4 5"); 
+  highAndLow("1 2 -3 4 5"); 
+  highAndLow("1 9 3 4 -5");
+
+  //ADVANCED TASK 5
+
+
+
+  //ADVANCED TASK 6
+
+  function getCode(str) {
+    let initialValue = 0;
+    let strArray = str.split("");
+    let total1 = strArray.map((item) => item.charCodeAt());
+    total1 = total1.join("");
+    let total2 = total1.replaceAll(7, 1);
+    let sumTotal1 = total1.split("").reduce((accumulator, currentValue) => +accumulator + +currentValue, initialValue);
+    let sumTotal2 = total2.split("").reduce((accumulator, currentValue) => +accumulator + +currentValue, initialValue);
+    return sumTotal1 - sumTotal2;
+  }
+
+  console.log(getCode("ABC"));
+  console.log(getCode("CCC"));
+
+  //ADVANCED TASK 7
