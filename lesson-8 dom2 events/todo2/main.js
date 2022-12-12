aee83da8d7e643209b5f488869c9b90954073542
xxaosens <vlad.obito@mail.ru>
2022-12-12 14:00:56 +0300

    добовление класса

--- lesson-8 dom2 events/todo2/main.js
+++ lesson-8 dom2 events/todo2/main.js
@@ -61,32 +61,71 @@ header__add.style.cssText = `
     border: 2px solid black;
     border-radius: 8px;
 `;
-////////////////////////
 
+///////////////////////////
 ///ДОБОВЛЕНИЕ НОВЫЪХ ЭЛЕМЕНТОВ В КОНТЕЙНЕР И ВЫВОД ИХ.
 header__add.onclick = () => {
     let header__input2 = header__input.value;//присваиваю переменной a2 содержимое инпут
     arr.push(header__input2);//пытаюсь содержимое input положить в пустой массив 
-///
+    ///
     console.log(arr)
-///
+    ///
     const taskHTML = `
         <div class="container" style="background-color: rgb(249, 213, 157); height: 80px; display: flex; justify-content: space-around; align-items: center; border: 2px solid black; border-radius: 8px; margin-bottom: 20px;">
 
-        <input type="checkbox" style="width: 25px; height: 25px;">
+        <input type="checkbox" class="container__status" data-action="done" style="width: 25px; height: 25px;">
 
         <div class="container_text" style="width: 250px; padding: 10px 0px; background-color: white; text-align: center; border-radius: 6px;">${header__input2}</div>
 
         <div class="container_column" style="height: 100%; display: flex; flex-direction: column; justify-content: space-around; align-items: flex-end;">
                 
-        <button style="width: 25px; height: 25px;">X</button></button>
+        <button data-action="button__delete" style="width: 25px; height: 25px;">X</button></button>
 
         <div class="container__date" style="background-color: rgb(0, 110, 153); padding: 2px 20px; border-radius: 4px;">Дата</div>
         </div>
         </div>
         `
+
+    ///ДОБОВЛЕНИЕ на страницу
     wrapper.insertAdjacentHTML("beforeend", taskHTML)
+
+    /// Очищаем поле ввода и возвращаем на него фокус
+    header__input.value = ""
+    header__input.focus()
+
 };
+///////////////////////////
+//УДАЛЕНИЕ ЗАДАЧИ
+wrapper.addEventListener("click", deleteTask)
+
+function deleteTask(event) {
+
+    if (event.target.dataset.action === "button__delete") {
+
+        const parenNode = event.target.closest(".container");
+
+        parenNode.remove()
+    }
+}
+///////////////////////////
+///ОТМЕЧЕНАЯ ЗАДАЧА И ДОБОВЛЕНИЯ НОВОГО КЛАССА
+wrapper.addEventListener("click", doneTask)
+
+function doneTask(event) {
+
+    if (event.target.dataset.action === "done") {
+
+        const parenNode = event.target.closest(".container");
+        const taskTitle = parenNode.querySelector(".container_text")
+        taskTitle.classList.toggle("container_text-done")
+        console.log(taskTitle)
+
+    }
+}
+///////////////////////////
+
+
+
 
 
 
@@ -136,7 +175,7 @@ for (let i = 0; i < arr.length; i++) {
     width: 25px;
     height: 25px;
     `;
-
+    
     let container__text = document.createElement("div");
     container__text.classList.add("container__text");
     container__text.innerHTML = `${arr[i]}`;
@@ -160,6 +199,7 @@ for (let i = 0; i < arr.length; i++) {
     `;
 
     let deleteButton = document.createElement("button");
+    // deleteButton.classList.add("button__delete");
     deleteButton.innerHTML = "X";
     deleteButton.style.cssText = `
         width: 25px;
