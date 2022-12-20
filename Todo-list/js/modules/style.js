e0143fc86f27af7d7b7c64a2eb43e327d96deea1
Taciana <saphronenka@gmail.com>
2022-12-20 19:37:01 +0300

    hw10

---
+++ Todo-list/js/modules/style.js
@@ -0,0 +1,116 @@
+export let cssRoot = `
+margin: 0 auto;
+width: 80%;
+background-color: rgb(193, 193, 193);
+border: 4px solid black;
+border-radius: 30px;
+`;
+
+export let cssWrapper = `
+width: 80%;
+height: 100px;
+margin: 0 auto;
+margin-top: 20px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+`;
+
+export let cssDeleteAllBtn = `
+width: 20%;
+height: 45px;
+justify-content: center;
+display: flex;
+align-items: center;
+border: 3px solid black;
+border-radius: 10px;
+background-color: rgb(34, 195, 195);
+font-size: 20px;
+`;
+
+export let cssInputTodo = `
+width: 50%;
+height: 45px;
+text-align: center;
+border: 3px solid black;
+border-radius: 10px;
+background-color: azure;
+color: gray;
+font-size: 20px;
+`;
+
+export let cssAddBtn = `
+width: 20%;
+height: 45px;
+justify-content: center;
+display: flex;
+align-items: center;
+border: 3px solid black;
+border-radius: 10px;
+background-color: rgb(34, 195, 195);
+font-size: 20px;
+`;
+
+export let cssContainer = `
+margin: 0 auto;
+width: 80%;
+height: 80px;
+display: flex;
+justify-content: space-between;
+border: 3px solid black;
+border-radius: 10px;
+`;
+
+export let cssCheckTodo = `
+margin-top: 22px;
+margin-left: 5px;
+width: 10%;
+height: 30px;
+justify-content: center;
+border: 2px solid;
+border-radius: 7px;
+color: rgb(34, 195, 195);
+`;
+
+export let cssTextCard = `
+margin-top: 22px;
+margin-left: 5px;
+width: 60%;
+height: 35px;
+justify-content: center;
+display: flex;
+align-items: center;
+border-radius: 5px;
+background-color: rgb(238, 237, 237);
+font-size: 20px;
+text-align: center;
+`;
+
+export let cssCard = `
+width: 16%;
+`;
+
+export let cssDeleteCardBtn = `
+margin-top: 10px;
+margin-left: 45px;
+width: 40px;
+height: 30px;
+display: flex;
+justify-content: center;
+align-items: center;
+border: 2px solid;
+border-radius: 7px;
+background-color: rgb(34, 195, 195);
+`;
+
+export let cssDateCard = `
+margin-top: 10px;
+width: 150px;
+height: 30px;
+justify-content: center;
+display: flex;
+align-items: center;
+border-radius: 3px;
+background-color: rgb(232, 231, 231);
+font-size: 20px;
+`;
